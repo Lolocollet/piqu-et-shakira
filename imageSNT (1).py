5cbcf8a0fdf737a5ec3a5093292a4c49943f37d6
Lolocollet <134008034+Lolocollet@users.noreply.github.com>
2023-05-19 18:26:59 +0200

    pique shakira
    traduction de monotia de shakira -
    Woh-oh-oh-oh-oh, eh
    Yeah
    
    [Shakira]
    Ce n'était ni ta faute, ni la mienne.
    C'était celle de la monotonie
    Je n'ai jamais rien dit, mais ça me faisait mal (Mmm, ah)
    Je savais que ça arriverait
    
    [Shakira]
    Tu fais ton truc, et moi également
    Toujours en quête de notoriété
    Tu as oublié ce qu'un jour on était
    Et le pire c'est que
    
    [Ozuna et Shakira]
    Ce n'était ni ta faute, ni la mienne.
    C'était celle de la monotonie
    Je n'ai jamais rien dit, mais ça me faisait mal (Mmm, ah)
    Je savais que ça arriverait (Oh-oh-oh)
    
    [Shakira & Ozuna]
    Soudain tu n'étais plus le même (Le même)
    Tu m'as quitté à cause de ton narcissisme (Égoïsme)
    Tu as oublié ce qu'un jour on était (Woh-oh, oh-oh)
    Hé-hé, hé
    
    [Shakira & Ozuna]
    Ton attitude distante augmentait mon inquiétude
    Tu ne donnais pas la moitié, mais je sais que j'ai donné plus que toi
    Je courais derrière quelqu'un qui ne faisait rien pour moi
    Cet amour n'est pas mort, mais il délire
    Ce qu'il y avait a disparu
    Je te le dis en toute sincérité
    Tu es froid comme une fin décembre
    Il vaut mieux que ce soit fini maintenant (Maintenant, maintenant)
    Ne me rejoue pas ce film encore une fois, je l'ai déjà vu celui-là (je l'ai déjà vu)
    Je t'aime, mais c'est que je m'aime plus (Plus moi)
    C'est un adieu nécessaire
    Ce qui était incroyable autrefois est devenu routine
    Tes lèvres n'ont aucun goût pour moi
    Maintenant c'est le contraire, et le pire c'est que
    
    [Shakira]
    Tu fais ton truc, et moi également
    Toujours à la recherche de notoriété
    Tu as oublié ce qu'un jour on était
    Et le pire c'est que
    
    [Shakira et Ozuna]
    Tu fais ton truc, et moi également
    Toujours à la recherche de notoriété
    Tu as oublié ce qu'un jour on était
    Hé-hé, hé
    
    [Ozuna]
    Ozuna, ah
    Shaki, je
    Hi Music Hi Flow

---
+++ imageSNT (1).py
@@ -0,0 +1,44 @@
+﻿from PIL import Image
+import os
+
+def traitement (triplet): # La fonction qui réalise le traitement des couleurs 'inversion
+
+    ancienRouge=triplet[0]
+    ancienVert=triplet[1]
+    ancienBleu=triplet[2]
+
+
+    nouveauRouge=ancienRouge
+    nouveauVert=0
+    nouveauBleu=0
+
+
+    return(nouveauRouge,nouveauVert,nouveauBleu)
+
+
+# Programme Principal
+
+imageDebut = Image.open("warhol.bmp") #Ouverture de l'image initiale.
+
+imageDebut.show() #Affichage de cette image.
+
+dimx = imageDebut.size[0]  #Récupération de ses dimensions.
+dimy = imageDebut.size[1]
+
+imageModifiee = Image.new('RGB', (dimx,dimy),(255,255,255)) # Création de l'image de sortie
+
+for i in range(0,dimx,1):
+    for j in range(0,dimy,1):
+        rvb=imageDebut.getpixel((i,j)) # On récupère le triplet des 3 composantes
+        rvbmodifie=traitement(rvb) # On inverse ce triplet.
+        imageModifiee.putpixel((i,j),(rvbmodifie[0],rvbmodifie[1],rvbmodifie[2]))
+
+imageModifiee.save("warholModifie.bmp", "bmp") # On enregistre l'image finale
+
+imageModifiee.show()
+
+imageDebut.close()
+
+imageModifiee.close()
+
+
